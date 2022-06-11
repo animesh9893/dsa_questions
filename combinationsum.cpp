961f9e0dabe5832f8b53319671e2b640ee9b45e2
animesh9893 <animesh.shrivatri2002@gmail.com>
2022-06-11 14:20:19 +0530

    combination sum leetcode solved

--- combinationsum.cpp
+++ combinationsum.cpp
@@ -1,40 +1,63 @@
 // https://leetcode.com/problems/combination-sum/
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-void push_emp(vector<int> &v){
-	for(int i=0;i<v.size();i++)
-		cout<<v[i]<<" ";
-	cout<<endl;
+#define max(a,b) (a<b? b:a)
+#define min(a,b) (a<b? a:b)
+#define mod 1e9+7
+#define FOR(a,c) for(int (a)=0;(a)<(c);(a)++)
+#define FORL(a,b,c) for(int (a)=(b);(a)<=(c);(a)++)
+#define FORR(a,b,c) for(int (a)=(b);(a)>=(c);(a)--)
+#define INF 1000000000000000003
+typedef long long int ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first
+#define S second
+#define PB push_back
+#define POB pop_back
+#define MP make_pair
+template <class T>
+T modadd(T a,T b,ll m){return (a%b+b%m)%m;}
+template <class T>
+T modsub(T a,T b,ll m){T ans = (a%m)-(b%m);if(ans<0) ans+=m;return ans;}
+template <class T>
+T modmul(T a,T b,ll m){ return ((a%m)*(b%m))%m; }
+vector<int> inputArray(int n){vector<int> arr(n);int i=0; FOR(i,n) cin>>arr[i];return arr;}
+
+void func(const int target,vector<int>&arr,int index,int sum_so_far,vector<int> &ans,vector<vector<int>> &result){
+	if(sum_so_far==target){
+		for(auto x:ans){
+			cout<<x<<" ";
+		}cout<<endl;
+
+		result.push_back(ans);
+		return ;
+	}
+	if(index==arr.size()) return ;
+	if(sum_so_far>target) return ;
+	ans.push_back(arr[index]);
+	func(target,arr,index,sum_so_far+arr[index],ans,result);
+	ans.pop_back();
+	func(target,arr,index+1,sum_so_far,ans,result);
+	return ;
 }
 
-int sum(vector<int> &v){
-	int x;
-	for(int i=0;i<v.size();i++)
-		x+=v[i];
-	return x;
-}
 
-void fun(std::vector<int> &emp,int i,vector<int> &nums,int target){
-	if(target==0)
-		push_emp(emp);	
-
-	if(i>nums.size()-1 or target<0 or target<nums[i])
-		return;
-	
-	emp.push_back(nums[i]);
-	fun(emp,i,nums,target-nums[i]);
-	emp.pop_back();
-
-
-	for(int j=1;j+i<nums.size();j++)
-		fun(emp,i+j,nums,target);
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+	vector<int> ans;
+	vector<vector<int>> res;
+	func(target,candidates,0,0,ans,res);
+	return res;
 }
 
 int main(){
-	vector<int> emp;
-	vector<int> nums={2,3,6,7};
-	int target = 7;
-	fun(emp,0,nums,target);
+	int n;cin>>n;
+	auto arr = inputArray(n);
+	cout<<"Targer\t";
+	int target;cin>>target;
+	combinationSum(arr,target);
+	cout<<endl;
+	return 0;
 }
