94f23fec1be251857b94e697f0e3f4bcf609e52b
animesh9893 <animesh.shrivatri2002@gmail.com>
2022-06-21 13:53:02 +0530

    push

--- gv.cpp
+++ gv.cpp
@@ -2,34 +2,17 @@
 
 using namespace std;
 
-#define max(a,b) (a<b? b:a)
-#define min(a,b) (a<b? a:b)
-#define mod 1e9+7
-#define FOR(a,c) for(int (a)=0;(a)<(c);(a)++)
-#define FORL(a,b,c) for(int (a)=(b);(a)<=(c);(a)++)
-#define FORR(a,b,c) for(int (a)=(b);(a)>=(c);(a)--)
-#define INF 1000000000000000003
-typedef long long int ll;
-typedef vector<int> vi;
-typedef pair<int,int> pi;
-#define F first
-#define S second
-#define PB push_back
-#define POB pop_back
-#define MP make_pair
-template <class T>
-T modadd(T a,T b,ll m){return (a%b+b%m)%m;}
-template <class T>
-T modsub(T a,T b,ll m){T ans = (a%m)-(b%m);if(ans<0) ans+=m;return ans;}
-template <class T>
-T modmul(T a,T b,ll m){ return ((a%m)*(b%m))%m; }
-vector<int> inputArray(int n){vector<int> arr(n);int i=0; FOR(i,n) cin>>arr[i];return arr;}
-
-
-
 int main(){
-    string s1,s2;cin>>s1>>s2;
-    cout<<(s1^s2)<<endl;
+    int n;cout<<"Enter number of questions : ";cin>>n;cout<<endl;
+
+    vector<string> arr(n);
+
+    for(int i=0;i<n;i++){
+        string s;cin>>s;
+        arr[i]=s;
+    }
     cout<<endl;
-    return 0;
+    for(auto x:arr){
+        cout<<"\""<<x<<"\", ";
+    }cout<<endl;
 }
